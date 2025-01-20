8f499db31cd192058b4524e07246b269458d5b89
Ashutosh9119 <asmednipur@gmail.com>
2025-01-20 12:09:38 +0530

    [Added: Implementation of Review Questions]

---
+++ Review/file1.sql
@@ -0,0 +1,37 @@
+ student_id | firstname | lastname | enrollment_date |           email           
+------------+-----------+----------+-----------------+---------------------------
+          1 | Ashutosh  | Singh    | 2004-03-25      | ashutosh@example.com
+          2 | Arhyam    | Malhotra | 0204-08-16      | arhyam@example.com
+          3 | Alice     | Johnson  | 2022-08-17      | alice.johnson@example.com
+(3 rows)
+
+ course_id | coursename  | credits 
+-----------+-------------+---------
+       101 | Mathematics |       4
+       102 | Physics     |       3
+       103 | Chemistry   |       4
+(3 rows)
+
+ enrollment_id | student_id | course_id | grade 
+---------------+------------+-----------+-------
+             1 |          1 |       101 |  9.00
+             2 |          1 |       102 |  8.50
+             3 |          1 |       103 |  7.50
+(3 rows)
+
+CREATE PROCEDURE
+ALTER TABLE
+ALTER TABLE
+ALTER TABLE
+CREATE SEQUENCE
+ALTER TABLE
+CALL
+ enrollment_id | student_id | course_id | grade 
+---------------+------------+-----------+-------
+             2 |          1 |       102 |  8.50
+             3 |          1 |       103 |  7.50
+             1 |          1 |       101 | 85.50
+(3 rows)
+
+CREATE FUNCTION
+CREATE TRIGGER
